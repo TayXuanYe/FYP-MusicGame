66b07257d046156136a4a1ddb5482d984cf52f51
TayXuanYe <pikaqiu04082004@gmail.com>
2025-08-31 18:17:46 +0800

    Add token authorize

--- Assets/Manager/UserDataManager.cs
+++ Assets/Manager/UserDataManager.cs
@@ -7,7 +7,7 @@ public partial class UserDataManager : Node
     public static UserDataManager Instance => _instance;
 
     public const string DEFAULT_USERNAME = "default_user";
-    public UserData CurrentUser { get; set; } = new UserData(-1, DEFAULT_USERNAME, "DEFAULT", false);
+    public UserData CurrentUser { get; set; } = new UserData(-1, DEFAULT_USERNAME, "DEFAULT", false, null);
 
     public override void _Ready()
     {
--- Assets/Model/UserData.cs
+++ Assets/Model/UserData.cs
@@ -1,15 +1,17 @@
 public class UserData
 {
-    public int Id { get; set; }
+    public int? Id { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
     public bool IsLoggedIn { get; set; }
+    public string AuthToken { get; set; }
 
-    public UserData(int id, string username, string email, bool isLoggedIn)
+    public UserData(int id, string username, string email, bool isLoggedIn, string authToken)
     {
         Id = id;
         Username = username;
         Email = email;
         IsLoggedIn = isLoggedIn;
+        AuthToken = authToken;
     }
 }
