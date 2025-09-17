ef5500820bb2e8ddb1b5e36bdc1160fb2ada6822
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-17 22:30:23 +0800

    result scene bind label and modify the ui

---
+++ Scene/ResultScene.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class ResultScene : Control
+{
+    [Export] private Label _titleLabel;
+    [Export] private Label _artistLabel;
+    [Export] private Label _difficultyLabel;
+    [Export] private Label _levelLabel;
+    [Export] private Label _scoreLabel;
+    [Export] private Label _maxComboLabel;
+    [Export] private Label _accuracyLabel;
+    [Export] private Label _finalAttentionLabel;
+    [Export] private Label _tapCriticalPerfectLabel;
+    [Export] private Label _tapPerfectLabel;
+    [Export] private Label _tapGreatLabel;
+    [Export] private Label _tapGoodLabel;
+    [Export] private Label _tapMissLabel;
+    [Export] private Label _holdCriticalPerfectLabel;
+    [Export] private Label _holdPerfectLabel;
+    [Export] private Label _holdGreatLabel;
+    [Export] private Label _holdGoodLabel;
+    [Export] private Label _holdMissLabel;
+
+    [Export] private Button _backButton;
+    [Export] private Button _nextButton;
+    [Export] private Button _exitButton;
+
+    
+}
