58aa8ef392bcb92bfd43e4f7929502cf6339e5ce
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-21 16:57:38 +0800

    Add new model for store gaze data

---
+++ Assets/Model/GazeData.cs
@@ -0,0 +1,15 @@
+public class GazeData
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public int Confidence { get; set; }
+    public double Timestamp { get; set; }
+
+    public GazeData(double x, double y, int confidence, double timestamp)
+    {
+        X = x;
+        Y = y;
+        Confidence = confidence;
+        Timestamp = timestamp;
+    }
+}
