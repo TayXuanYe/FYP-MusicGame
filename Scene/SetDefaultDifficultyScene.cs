e066c27f8ad8f788f4c76e202bb53a8914f60196
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-25 15:42:54 +0800

    And add script for SetDefaultDifficultyScene

---
+++ Scene/SetDefaultDifficultyScene.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class SetDefaultDifficultyScene : Control
+{
+    [Export] private Button _basicButton;
+    [Export] private Button _advancedButton;
+    [Export] private Button _expertButton;
+
+    public override void _Ready()
+    {
+        _basicButton.Pressed += OnBasicButtonPressed;
+        _advancedButton.Pressed += OnAdvancedButtonPressed;
+        _expertButton.Pressed += OnExpertButtonPressed;
+    }
+
+    private void OnBasicButtonPressed()
+    {
+        UserDataManager.Instance.CurrentUser.SuggestedDifficulty = "Basic";
+        SceneManager.Instance.ChangeToMainMenuScene();
+    }
+
+    private void OnAdvancedButtonPressed()
+    {
+        UserDataManager.Instance.CurrentUser.SuggestedDifficulty = "Advanced";
+        SceneManager.Instance.ChangeToMainMenuScene();
+    }
+
+    private void OnExpertButtonPressed()
+    {
+        UserDataManager.Instance.CurrentUser.SuggestedDifficulty = "Expert";
+        SceneManager.Instance.ChangeToMainMenuScene();
+    }
+}
