31b888f6b2e3f30daf0c488a96627a5ae40e2e81
TayXuanYe <pikaqiu04082004@gmail.com>
2025-10-05 17:50:33 +0800

    feat: add loading component with SVG icon and scene configuration

---
+++ Scene/LoadingComponent.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class LoadingComponent : Control
+{
+	[Export] private TextureRect _loadingIcon;
+	public override void _Ready()
+	{
+		Vector2 size = _loadingIcon.Size;
+		
+		_loadingIcon.PivotOffset = size / 2.0f;
+	}
+	public override void _Process(double delta)
+	{
+		_loadingIcon.Rotation += (float)(delta * 3); // Rotate at 2 radians per second
+	}
+
+}
